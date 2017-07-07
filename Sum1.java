e999d5ab30571025792456185fdb97ecdd7c019f
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-07-07 10:12:12 +0530

    Add files via upload

---
+++ Sum1.java
@@ -0,0 +1,22 @@
+package Guvi;
+
+public class Sum1 {
+
+	public static void main(String[] args) {
+		int temp1=0,temp=0;
+		for(int i=1;i<15;i++){
+		temp1=temp1+i;
+		}
+		System.out.println(temp1);
+		for(int i=15;i<45;i++){
+		if(i%2!=0){
+			temp=temp+i;
+		}
+				
+		}
+		System.out.println(temp);
+		
+
+	}
+
+}

c07dcd32bcb432976d4109cb9f4ce10d046a4292
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-07-07 10:15:31 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,15 @@
+package Guvi;
+import java.util.Scanner;
+public class Count {
+
+	public static void main(String[] args) {
+		
+		String str;
+		Scanner s=new Scanner(System.in);
+		str=s.nextLine();
+		int len=str.length();
+		System.out.println(+len);
+		
+	}
+
+}
